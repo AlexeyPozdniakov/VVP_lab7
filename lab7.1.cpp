29e3dd975bb74cb9e71a9c3348b7843f0f3b788e
AlexSmith1234 <54896619+AlexSmith1234@users.noreply.github.com>
2020-11-09 17:41:48 +0300

    Лабораторная 7

---
+++ lab7.1.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+using namespace std;
+int main()
+{
+    cout<<"Input numbers"<<endl;
+    float a,b;
+    cin>>a>>b;
+    if ((a>3)&&(b<=3)) {
+        cout<<"True";
+    }
+    else {
+        cout<<"False";
+    }
+}
